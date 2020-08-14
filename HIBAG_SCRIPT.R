630438cdd0dbb96735aa7b1aa9d53cece6fd123e
Aditya Ambati <ambati@stanford.edu>
2020-08-14 12:38:22 -0700

    Update HIBAG_SCRIPT.R
    added output parsing and exit conditions

--- HIBAG_SCRIPT.R
+++ HIBAG_SCRIPT.R
@@ -1,16 +1,36 @@
+## author : ambati@stanford.edu
 arg=commandArgs(trailingOnly = T)
+#arg1 = model
+#arg2 = plink bed files in hg19 coordinates
+#arg3 = output file
 #'''load the libraries and setup a cluster'''
-require(HIBAG)
+sink(paste0(arg[3], '.log'))
+PackList = rownames(installed.packages())
+if('data.table' %in%  PackList & 'parallel' %in% PackList){
+  require(data.table)
+  require(ggplot2)
+  require(stringr)
+} else {
+  install.packages('data.table')
+}
 library(parallel)
 require(data.table)
+if('HIBAG' %in% PackList){
+  require(HIBAG)
+} else {
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  BiocManager::install("HIBAG")
+  require(HIBAG)
+}
+### make n-1 clusters
 n.cores = detectCores() -1
 cl <- makeCluster(n.cores)
-model.list <- get(load(arg[1]))
+model.list <- get(load(arg[1])) 
 yourgeno = hlaBED2Geno(bed.fn = paste0(arg[2], ".bed"),
                        bim.fn = paste0(arg[2], ".bim"),
                        fam.fn = paste0(arg[2], ".fam"))
 summary(yourgeno)
-
 output_pred_QC=list()
 hlas_id = names(model.list)
 #'''predict function to wrap in a lapply call'''
@@ -18,18 +38,27 @@ pred_hibag=function(hla, yourgeno, model.list){
   model = hlaModelFromObj(model.list[[hla]])
   print(model)
   pred.guess = predict(model, yourgeno, type = "response+prob", cl=cl, verbose = T, match.type="Position")
-  return(pred.guess)
+  cat('PREDICTED ', hla, timestamp())
+  return(pred.guess$value)
 }
 
 output_pred_QC=lapply(hlas_id, pred_hibag,  yourgeno = yourgeno, model.list = model.list)
 names(output_pred_QC) = hlas_id
-
-#'''write to current dir function to wrap in a lapply call'''
-hibag_extract=function(hla.num, output_pred_QC, filename){
-  fwrite(output_pred_QC[[hla.num]]$value, file = paste0('IMPUTED_', output_pred_QC[[hla.num]]$locus, "_", filename, ".txt"), row.names = T)
-  fwrite(as.data.frame(output_pred_QC[[hla.num]]$postprob), file= paste0('POSTERIORS_', output_pred_QC[[hla.num]]$locus, "_",filename, ".txt"), row.names = T)
-
+## stop the parallel cluster
+stopCluster(cl = cl)
+## combined the outputs and write out
+combinedPreds =data.frame(sample.id=output_pred_QC[[1]]$sample.id)
+for(i in seq(1:length(model.list))){
+  hla.names = c(paste0(hlas_id[i], '.1'), paste0(hlas_id[i], '.2'), paste0(hlas_id[i], '.prob'))
+  temp = output_pred_QC[[i]]
+  names(temp)[2:4] = hla.names
+  if (identical(combinedPreds$sample.id, temp$sample.id)){
+    combinedPreds=cbind.data.frame(combinedPreds, temp[, 2:4])
+  } else {
+    stop('SAMPLE IDS are not in same order check !!')
+  }
 }
-
-lapply(1:length(output_pred_QC),  hibag_extract, output_pred_QC = output_pred_QC, filename=arg[3])
-
+print(dim(combinedPreds))
+## write out the file
+fwrite(combinedPreds, file=arg[3])
+sink()
